a8b71dd2e48637d1bcc4a0451e27ca892655cb9b
Weinne Santos <weinnewillan@gmail.com>
2025-01-23 18:09:58 -0300

    Initial commit

---
+++ src/main/java/com/weinne/finance_system/FinanceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.weinne.finance_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceSystemApplication.class, args);
+	}
+
+}
